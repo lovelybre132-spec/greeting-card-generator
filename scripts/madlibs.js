1e604f3d7578bee64401824b5f01ebb7546c7815
brittany <lovelybre132@gmail.com>
2026-09-13 11:27:47 -0500

    added variables to madlibs.js

---
+++ scripts/madlibs.js
@@ -0,0 +1,17 @@
+let  name = prompt("A name:");
+let adjective = prompt("An adjective:");
+let noun = prompt("A noun:");
+let verb = prompt("A verb:");
+let name2 = prompt("Name 2:");
+//let  name = "Girly";
+//let adjective = "funny";
+//let noun = "ball";
+//let verb = "run";
+//let name2 = "Tony";
+console.log("Dear " + name + ",");
+console.log("You are as " + adjective + " as a " + noun + ".");
+console.log("Happy birthday from the generator.");
+
+console.log("Hey " + name + ",");
+console.log("Can you come " + verb + " with " + name2);
+console.log(" to hang out.");
